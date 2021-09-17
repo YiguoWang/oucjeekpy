f45ef95d68b9086ce1ccacff9dce17af3b00f9ef
YiguoWang <282832606@qq.com>
2021-09-17 20:41:38 +0800

    hello

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .core import *
+
+print('run init!')
---
+++ core.py
@@ -0,0 +1,16 @@
+# Insert your code here. 
+
+import numpy as np
+
+def jeekprint():
+    print("hello world,python")
+    
+    
+def print_hi():
+    print("hi")
+
+def main():
+    pass
+    
+if __name__=='__main__':
+    main()
